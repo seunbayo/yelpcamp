e09e95253986a2ee64e02224d45706625a131fae
Pablo Brasero <pablo@pablobm.com>
2020-07-30 11:02:14 +0100

    Render with HTML layout

--- Api/index.js
+++ Api/index.js
@@ -3,8 +3,12 @@ const pokemon = require('./pokemon');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  let onSuccess = (_response, body) => res.send(body);
+  let onSuccess = (_response, body) => {
+    res.render('index', { result: body });
+  }
   let onError = error => res.send(error)
   pokemon.all(onSuccess, onError)
 });
