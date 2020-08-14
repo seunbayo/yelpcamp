55fec9ee2a60dc804d6c8d259aa6ce6b9fa7da96
seunbayo <seunbayo83@gmail.com>
2020-08-14 22:56:00 +0100

    styled navbar and form

--- v3/app.js
+++ v3/app.js
@@ -49,13 +49,13 @@ app.get("/campgrounds", function(req, res){
             res.render("index", {campgrounds:allCampgrounds});
         }
     });
-})
+});
 
 app.post("/campgrounds", function(req, res){
     // get data from form and add to array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image:image, description: desc}
+    var newCampground = {name: name, image:image, description: description}
     //create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -67,11 +67,28 @@ app.post("/campgrounds", function(req, res){
     });
 });
 
+//NEW - show form to create a new campground
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs");
 });
 
+//SHow - show more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with the provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
+});
 
-app.listen(3000, function() {
+
+
+
+
+app.listen(4000, function() {
     console.log("Yelp camp server");
 });
