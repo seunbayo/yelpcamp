665b4198b0fa14c4d985e80e9d8e5d2174585019
seunbayo <seunbayo83@gmail.com>
2020-08-18 12:21:58 +0100

    styled the show page

--- v5/seeds.js
+++ v5/seeds.js
@@ -1,25 +1,26 @@
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
-var Comment    = require("./models/comment");
+var Comment = require("./models/comment");
 
 var data = [
   {
     name: "Olive mountain ",
     image:
       "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350",
-    description: "hulabalolsoskdcdinficsedsdf",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
   },
   {
     name: "Oliverr superb",
     image:
       "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350",
-    description: "hulabalolsoskdcdinficsedsdf",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
   },
   {
     name: "Obudu resortee",
     image:
       "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350",
-    description: "hulabalolsoskdcdinficsedsdf",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
   },
 ];
 
@@ -32,28 +33,31 @@ function seedDB() {
     console.log("removed campground");
     //add a few campgrounds
     data.forEach(function (seed) {
-        Campground.create(seed, function (err, campground) {
-            if (err) {
+      Campground.create(seed, function (err, campground) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("added a campground");
+          //create a comment
+          Comment.create(
+            {
+              text: "This is a fantastic place to be",
+              author: "Homeboy",
+            },
+            function (err, comment) {
+              if (err) {
                 console.log(err);
-            }  else {
-                console.log("added a campground");
-                //create a comment
-                Comment.create(
-                {
-                    text: "This is a fantastic place to be",
-                    author: "Homeboy"
-                },  function(err, comment){
-                        if(err){
-                            console.log(err);
-                        }   else {
-                            campground.comments.push(comment);
-                            campground.save();
-                            console.log('created new comment ');
-                        }
-                })  }
-            });
-        });
-    })
+              } else {
+                campground.comments.push(comment);
+                campground.save();
+                console.log("created new comment ");
+              }
+            }
+          );
+        }
+      });
+    });
+  });
 }
 
 module.exports = seedDB;
