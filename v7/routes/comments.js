ed2611bb2a7189719f4a3d76944da4ce42d7fe2a
Pablo Brasero <pablo@pablobm.com>
2020-08-29 13:19:17 +0100

    Import correct module

--- v7/routes/comments.js
+++ v7/routes/comments.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router({ mergeParams: true });
-var Campground = require("../models/comment");
+var Campground = require("../models/campground");
 var Comment = require("../models/comment");
 var middleware = require("../middleware/index.js");
 
