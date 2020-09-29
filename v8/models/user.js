b53213b1a7cec12659cc7a899bbc979ad0f7561d
seunbayo <seunbayo83@gmail.com>
2020-09-29 13:06:23 +0100

    modified model for validations

--- v8/models/user.js
+++ v8/models/user.js
@@ -2,10 +2,20 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
+  username: {
+    type: String,
+    required: [true, "Name is required"],
+    unique: true,
+    min: 5
+  },
+  email: {
+      type: String,
+      lowercase: true,
+      required: [true, "Email is required"],
+      minlength: [10, "Email can't be shorter than 10 characters"],
+      maxlength: [50, "Email can't be longer than 50 characters"],
+  }
 });
-
 
 UserSchema.plugin(passportLocalMongoose);
 
